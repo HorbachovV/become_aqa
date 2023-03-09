d991b88ff812834e1dce05c7b4758d2581d0d19f
Volodymyr Horbachov <gorbacovvolodimir@gmail.com>
2023-03-09 19:52:11 +0200

    add status code 200 check

--- tests/test_test.py
+++ tests/test_test.py
@@ -1,3 +1,8 @@
 # to run test pytest .
+import pip._vendor.requests as requests
 def test_test():
     assert 1 == 1
+
+def test_api_response_code():
+    response = requests.get('https://github.com/HorbachovV/become_aqa')
+    assert response.status_code == 200
