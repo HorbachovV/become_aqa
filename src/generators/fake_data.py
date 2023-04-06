8350ba1ceb492d7f6f5cd55614053eb687987f92
Volodymyr Horbachov <gorbacovvolodimir@gmail.com>
2023-04-06 10:03:59 +0300

    add test_search_not_working_without_q

--- src/generators/fake_data.py
+++ src/generators/fake_data.py
@@ -1,9 +1,9 @@
 from faker import Faker
 
-fake = Faker()
+fake = Faker('en_US')
 
 class FakeData:
 
     def random_repo_name(self):
-        repo_name = fake.url()
+        repo_name = fake.text()
         return repo_name
--- tests/test_search_repo.py
+++ tests/test_search_repo.py
@@ -1,8 +1,10 @@
 import requests
 from src.applications.api.github_api import GitHubApiClient
 from src.generators.fake_data import FakeData
+from src.config.config import config
+from src.validation.checker import Check
 
-
+github_api_url = config.get("GITHUB_BASE_API")
 
 def test_existing_repo_can_be_found():
     client = GitHubApiClient()
@@ -20,4 +22,6 @@ def test_non_existing_repo_can_not_be_found():
 
     
 def test_search_not_working_without_q():
-    pass
+    r = requests.get(f"{github_api_url}search/repositories")
+    response = Check(r)
+    response.assert_status_code(422).assert_request_method("GET")
